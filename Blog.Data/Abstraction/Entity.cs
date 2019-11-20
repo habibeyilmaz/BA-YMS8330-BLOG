991295c18a97d78bf51d96c95f544c402f41e1f2
habibe.yilmaz <habibeezgiyilmaz@gmail.com>
2019-11-20 21:24:56 +0300

    Genel yapı 1#

---
+++ Blog.Data/Abstraction/Entity.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Blog.Data.Models;
+using System;
+
+namespace Blog.Data.Abstraction
+{
+    public abstract class Entity
+    {
+
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Deleted { get; set; }
+
+
+
+    }
+}
---
+++ Blog.Data/Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Data.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Models
+{
+    public class Blog : Entity
+    {
+    }
+}
