042077209fe74b70a87bfd61b0920f2904b7e0ff
habibe.yilmaz <habibeezgiyilmaz@gmail.com>
2019-11-25 21:27:27 +0300

    25.11.2019

---
+++ Blog.Data/Dto/ContactSendDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Dto
+{
+    public class ContactSendDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Message { get; set; }
+    }
+}
