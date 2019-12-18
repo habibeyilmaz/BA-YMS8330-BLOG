5f98731060ef07362aac499c4440526eff4c7de7
habibe.yilmaz <habibeezgiyilmaz@gmail.com>
2019-12-18 21:33:45 +0300

    deneme

---
+++ yen/WebApplication10/WebApplication10/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Dto;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Demo([FromBody] DemoDto demo)
+        {
+            return View(demo);
+        }
+
+        public IActionResult Name([FromBody] DemoDto name)
+        {
+            return View(name);
+        }
+    }
+}
